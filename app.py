f9b334dad3f66363db08465ad7f3e877ed3cfcd5
Edwin Philip <p.edwin200294@gmail.com>
2020-09-23 18:38:21 +0530

    feat: adding exception handlers and pytest module

--- app.py
+++ app.py
@@ -1,21 +1,38 @@
 """Application entry point."""
-import os
+import os, requests, sys
 from flask import Flask, request, jsonify
-from webcrawler import getWebsiteAssets
+from methods import getWebsiteAssets
 from dotenv import *
 
 load_dotenv()
 
 app = Flask(__name__)
 
+def handlers(app):
 
-@app.route('/', methods=['POST'])
-def getImages():
-    data = request.get_json()
-    if getWebsiteAssets(data["url"]):
-        return 201
-    else:
-        return 500
+    @app.route('/')
+    def getroute():
+        return "Hello World", 200
+
+
+    @app.route('/', methods=['POST'])
+    def getImages():
+        data = request.get_json()
+        if data and data.get("url"):
+            try:
+                if getWebsiteAssets(data["url"]):
+                    res = { "status": "201","message": "Images Downloaded"}
+                    return jsonify(res), 201
+            except Exception as e:
+                res = { "status": "500", "message" : "{}".format(e) }
+                return jsonify(res), 500
+
+        else:
+            res = { "status": "400", "message" : "Invalid Payload, Expecting parameter \'url\'" }
+            return jsonify(res), 400
+
+
+handlers(app)
 
 if __name__ == "__main__":
     if os.getenv('ENVIRONMENT') == 'DEVELOPMENT':
