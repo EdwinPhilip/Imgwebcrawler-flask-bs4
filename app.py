38a797329104a0305e2d404784049bb06525cf61
Edwin Philip <p.edwin200294@gmail.com>
2020-09-23 14:54:28 +0530

    feat: fixing flask app host to run in docker

--- app.py
+++ app.py
@@ -19,6 +19,6 @@ def getImages():
 
 if __name__ == "__main__":
     if os.getenv('ENVIRONMENT') == 'DEVELOPMENT':
-        app.run(host='127.0.0.1',port=os.getenv('PORT'),debug=True)
+        app.run(host='0.0.0.0',port=os.getenv('PORT'),debug=True)
     elif os.getenv('ENVIRONMENT') == 'PRODUCTION' or not os.getenv('ENVIRONMENT'):
         app.run(host='0.0.0.0',port='80')
